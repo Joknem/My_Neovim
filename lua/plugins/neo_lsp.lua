f0b61ab1fb1eb8994fa57af1d61c60f7cd1eeabc
joknem <2501204180@qq.com>
2024-03-07 16:06:48 +0800

    config R

--- lua/plugins/neo_lsp.lua
+++ lua/plugins/neo_lsp.lua
@@ -11,7 +11,7 @@ return {
       local servers = {
         bashls = {},
         clangd = { cmd = { "clangd", "--offset-encoding=utf-16", } },
-        r_language_server = { cmd = {"radian"}},
+        r_language_server = {},
         jsonls = {},
         pyright = {},
         lua_ls = {
