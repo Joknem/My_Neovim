e3c9eb4a57866b76e9b7733c44f159f29393c1df
joknem <2501204180@qq.com>
2024-06-24 14:03:26 +0800

    change theme twilight

--- lua/plugins/neo_ui.lua
+++ lua/plugins/neo_ui.lua
@@ -1,15 +1,13 @@
 return {
   {
-    'Kicamon/gruvbox.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd('colorscheme gruvbox')
-      vim.fn.sign_define('DiagnosticSignError', { text = '🤣', texthl = 'DiagnosticSignError' })
-      vim.fn.sign_define('DiagnosticSignWarn', { text = '🧐', texthl = 'DiagnosticSignWarn' })
-      vim.fn.sign_define('DiagnosticSignInfo', { text = '🫠', texthl = 'DiagnosticSignInfo' })
-      vim.fn.sign_define('DiagnosticSignHint', { text = '🤔', texthl = 'DiagnosticSignHint' })
-    end,
+    "folke/twilight.nvim",
+    config = function ()
+      require("twilight").setup(
+      {
+        terminal = true,
+      }
+      )
+    end
   },
   {
     "nvim-tree/nvim-tree.lua",
