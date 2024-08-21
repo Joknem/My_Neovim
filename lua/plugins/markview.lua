e5f7a669db3bfd0cebf71ef064aa9abeda907044
joknem <2501204180@qq.com>
2024-08-21 22:45:22 +0800

    8.21

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,6 +1,6 @@
 return {
     "OXY2DEV/markview.nvim",
-    ft = { "md" },
+    ft = "markdown",
 
     dependencies = {
         -- You may not need this if you don't lazy load
@@ -9,25 +9,4 @@ return {
 
         "nvim-tree/nvim-web-devicons"
     },
-    config = function ()
-      require("markview").setup({
-    buf_ignore = { "nofile" },
-    modes = { "n", "no" },
-
-    options = {
-        on_enable = {},
-        on_disable = {}
-    },
-
-    block_quotes = {},
-    checkboxes = {},
-    code_blocks = {},
-    headings = {},
-    horizontal_rules = {},
-    inline_codes = {},
-    links = {},
-    list_items = {},
-    tables = {}
-});
-    end
 }
